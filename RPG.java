1867096a90a3559d906e132ec016ac8cc8eb66ea
tethys89 <tethys_git@hotmail.com>
2013-11-01 20:45:04 +0000

    command parsing using 'if' instead of 'case'

--- RPG.java
+++ RPG.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Arrays;
 
 public class RPG {
 
@@ -59,52 +60,129 @@ public class RPG {
 	currentLoc.printExits();
 
 	BufferedReader command = new BufferedReader(new InputStreamReader(System.in));
-	String orders = null;
+	String orders = "";
+
+
 	while (true) {
+	    //<editor-fold defaultstate="collapsed" desc="ask for command">	    
 	    System.out.print("What do you want to do? ");
 	    try {
 		orders = command.readLine();
 	    } catch (IOException ioe) {
 		System.out.println("I'm sorry, I didn't understand that!");
-		System.exit(1);
+		//System.exit(1);
 	    }
+	    //split string into array
+	    String[] orderWords = orders.toUpperCase().split(" ");
+	    //turn string[] into arraylist
+	    ArrayList<String> orderList = new ArrayList<>(Arrays.asList(orderWords));
 
-	    String[] orderWords = orders.split(" ");
+	    //</editor-fold>
 
-//	    for (String s : orderWords){
-//		System.out.print(s);
-	    switch (orderWords[0].toLowerCase()) {
-		case "go":
-		    int count = 0;
-		    for (Exit e : currentLoc.getExits()) {
-			String direct = orderWords[1].toUpperCase();
-			if (direct.equals(e.getDirectionName())) {
-			    currentLoc = e.getLeadsTo();
+	    //<editor-fold defaultstate="collapsed" desc="switchable using string[]">
+	     /* switch (orderWords[0].toLowerCase()) {
+	     * case "go":
+	     * int count = 0;
+	     * for (Exit e : currentLoc.getExits()) {
+	     * String direct = orderWords[1].toUpperCase();
+	     * if (direct.equals(e.getDirectionName())) {
+	     * currentLoc = e.getLeadsTo();
+	     * count++;
+	     * System.out.print("You are standing in the " + currentLoc.getName() + "\n" + currentLoc.getDesc() + "\n");
+	     * currentLoc.printInventory();
+	     * currentLoc.printExits();
+	     * break;
+	     * }
+	     * }
+	     * if (count == 0) {
+	     * System.out.print("I'm sorry, I can't go that way.\n");
+	     * }
+	     * break;
+	     * 
+	     * case "pick":
+	     * 
+	     * 
+	     * case "put":
+	     * 
+	     * 
+	     * case "exit":
+	     * System.exit(0);
+	     * break;
+	     * } 
+	     */
+	    //</editor-fold>
+
+	    //<editor-fold defaultstate="collapsed" desc="arrayList if">
+	    //<editor-fold defaultstate="collapsed" desc="GO">
+	    if (orderList.contains("GO")) {
+		int count = 0;
+		for (Exit e : currentLoc.getExits()) {
+		    if (orderList.contains(e.getDirectionName())) {
+			currentLoc = e.getLeadsTo();
+			count++;
+			System.out.print("You are standing in the " + currentLoc.getName() + "\n" + currentLoc.getDesc() + "\n");
+			currentLoc.printInventory();
+			currentLoc.printExits();
+			break;
+		    }
+		}
+		if (count == 0) {
+		    System.out.print("I'm sorry, I can't go that way.\n");
+		}
+	    }
+	    //</editor-fold>
+
+	    else if (orderList.contains("TAKE") || orderList.contains("PICK")) {
+		//counter for picked up items
+		int count = 0;
+		//loop over items in the room
+		for (Item i : currentLoc.getInvent()) {
+		    //split the item name into string[]
+		    String[] itemName = i.getName().toUpperCase().split(" ");
+		    //loop over the words in the item name
+		    for (String s : itemName) {
+			//test if the word in the item name is contained in the order
+			//(ignore 'a', 'the' etc?)
+			if (orderList.contains(s) && !s.equals("A") && !s.equals("THE")) {
+			    Avatar.pickUpItem(i.getName(), currentLoc);
+			    Avatar.printInventory();
 			    count++;
-			    System.out.print("You are standing in the " + currentLoc.getName() + "\n" + currentLoc.getDesc() + "\n");
-			    currentLoc.printInventory();
-			    currentLoc.printExits();
 			    break;
 			}
 		    }
-		    if (count == 0) {
-			System.out.print("I'm sorry, I can't go that way.\n");
+		}
+		if (count == 0){System.out.print("I'm sorry I can't pick that up\n");}
+	    }
+	    
+	    else if(orderList.contains("LEAVE") || orderList.contains("DROP") || orderList.contains("PUT")){
+		
+		//counter for picked up items
+		int count = 0;
+		//loop over items in the room
+		for (Item i : Avatar.getInvent()) {
+		    //split the item name into string[]
+		    String[] itemName = i.getName().toUpperCase().split(" ");
+		    //loop over the words in the item name
+		    for (String s : itemName) {
+			//test if the word in the item name is contained in the order
+			//(ignore 'a', 'the' etc?)
+			if (orderList.contains(s) && !s.equals("A") && !s.equals("THE")) {
+			    Avatar.putDownItem(i.getName(), currentLoc);
+			    Avatar.printInventory();
+			    count++;
+			    break;
+			}
 		    }
-		    break;
-
-		case "pick":
-
-
-		case "put":
-
-
-		case "exit":
-		    System.exit(0);
-		    break;
+		}
+		if (count == 0){System.out.print("I'm sorry I can't put that down\n");}
 	    }
 
+		if (orderList.contains("QUIT")) {
+		    System.exit(0);
+		}
+		//</editor-fold>
+	    }
+
+
 	}
-
-
     }
-}
